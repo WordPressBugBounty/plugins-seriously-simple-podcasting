afb5404b7eff02dad50114bc56ba60f3731f8f20
Sean Murphy <sean@iamseanmurphy.com>
2026-05-21 15:19:25 +0000

    3.16.1

--- seriously-simple-podcasting/vendor/composer/installed.php
+++ seriously-simple-podcasting/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'castos/seriously-simple-podcasting',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '5b9fee89750e8b15a035f2f23c5402ada7e7d2b7',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '6217c14046a5af1b3cfd5042e0713460be6b92d4',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'castos/seriously-simple-podcasting' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '5b9fee89750e8b15a035f2f23c5402ada7e7d2b7',
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '6217c14046a5af1b3cfd5042e0713460be6b92d4',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
